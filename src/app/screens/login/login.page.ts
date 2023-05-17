0db66e7b87036fdd4e41a6164fef65f6937a7dca
I Fero I <106333313+IFero04@users.noreply.github.com>
2023-05-17 23:49:47 +0100

    Save

--- src/app/screens/login/login.page.ts
+++ src/app/screens/login/login.page.ts
@@ -1,20 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule]
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  isLogedIn: boolean;
+
+  constructor(private router: Router) { 
+    this.loginForm =  new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(2)])
+    });
+    this.isLogedIn = false;
+  }
 
   ngOnInit() {
   }
 
+  goBack() {
+    this.router.navigate([''])
+  }
+
+  login() {
+    this.isLogedIn = true;
+    if (!this.loginForm.valid) {
+      return false;
+    } else {
+      console.log(this.loginForm.value);
+      return true;
+    }
+  }
+
+  get formControls() {
+    return this.loginForm.controls;
+  }
 }
--- src/app/screens/register/register.page.ts
+++ src/app/screens/register/register.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -12,9 +13,17 @@ import { IonicModule } from '@ionic/angular';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+  
+  goLogin() {
+    this.router.navigate(['/login'])
+  }
+
+  goBack() {
+    this.router.navigate([''])
   }
 
 }
